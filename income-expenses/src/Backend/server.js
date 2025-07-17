eb303bd33ba6ea187db1dc41a435a5f3c19fd546
hamashua1 <joshuahamashua@gmail.com>
2025-07-17 22:45:21 +0000

    files updated

---
+++ income-expenses/src/Backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+const app = express()
+const PORT = 8000
+app.listen = PORT
+app.use (cors())
+
+
+app.get('/api/', async (req,res) => {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
