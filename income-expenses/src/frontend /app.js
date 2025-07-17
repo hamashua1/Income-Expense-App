8cc93aeb8d620baf5e6b8c4ced5de70fc3d86430
hamashua1 <joshuahamashua@gmail.com>
2025-07-17 17:30:16 +0000

    files updated

--- income-expenses/src/frontend /app.js
+++ income-expenses/src/frontend /app.js
@@ -1,8 +1,30 @@
 
+// Adding User inputs 
+function add(){
+const title = document.getElementById("title").value
+const amount = Number(document.getElementById("amount").value)
+const type = document.getElementById("type").value
+const date = document.getElementById("date").value
+const display = document.getElementById("output")
 
-<script>
+const money = JSON.parse(localStorage.getItem('money')) || []
+display.innerHtml = ""
+money.push({title, amount, type, date})
+localStorage.setItem('money', JSON.stringify(money))
+document.getElementById("title").value = ""
+document.getElementById("amount").value = ""
+document.getElementById("type").value = ""
+document.getElementById("date").value = ""
+}
 
-</script>
+
+
+//Showing User Inputs
+function expense(){
+
+
+
+}
 
 
 
