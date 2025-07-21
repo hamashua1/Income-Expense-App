2bbf7e8c39fd11954ae37a3be75b79dcf807035e
hamashua1 <joshuahamashua@gmail.com>
2025-07-21 19:15:11 +0000

    file updated

--- income-expenses/src/Backend/server.js
+++ income-expenses/src/Backend/server.js
@@ -13,7 +13,7 @@ const PORT = 8000
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, './frontend')));
-setupDatabase()
+await setupDatabase()
 
 // Database connection
 async function getDatabase() {
